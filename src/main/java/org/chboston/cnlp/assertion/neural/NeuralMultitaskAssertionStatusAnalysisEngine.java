be62097f0e82488dbcaace4a15f4818f89d089d7
tmill <tim.miller@gmail.com>
2016-09-13 10:27:08 -0400

    Add possibility of using extractor constrained by sentences.

--- src/main/java/org/chboston/cnlp/assertion/neural/NeuralMultitaskAssertionStatusAnalysisEngine.java
+++ src/main/java/org/chboston/cnlp/assertion/neural/NeuralMultitaskAssertionStatusAnalysisEngine.java
@@ -8,6 +8,7 @@ import org.apache.ctakes.neural.feature.TokensSequenceWithWindowExtractor;
 import org.apache.ctakes.typesystem.type.textsem.EntityMention;
 import org.apache.ctakes.typesystem.type.textsem.EventMention;
 import org.apache.ctakes.typesystem.type.textsem.IdentifiedAnnotation;
+import org.apache.ctakes.typesystem.type.textspan.Sentence;
 import org.apache.uima.UIMAFramework;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.util.JCasUtil;
@@ -24,51 +25,54 @@ public class NeuralMultitaskAssertionStatusAnalysisEngine extends
 
   private final static int WINDOW_SIZE = 10;
   private TokensSequenceWithWindowExtractor seqExtractor = new TokensSequenceWithWindowExtractor(WINDOW_SIZE);
+//  private TokenSequenceWithConstrainedWindowExtractor<IdentifiedAnnotation, Sentence> seqExtractor = new TokenSequenceWithConstrainedWindowExtractor<>(WINDOW_SIZE);
     
   @Override
   public void process(JCas jCas) throws AnalysisEngineProcessException {
-    
-    Collection<IdentifiedAnnotation> entities = JCasUtil.select(jCas, IdentifiedAnnotation.class);
-    for (IdentifiedAnnotation identifiedAnnotation : entities)
-    {
-      if (!(identifiedAnnotation instanceof EntityMention || identifiedAnnotation instanceof EventMention))
+    for(Sentence sent : JCasUtil.select(jCas, Sentence.class)){
+      Collection<IdentifiedAnnotation> entities = JCasUtil.selectCovered(jCas, IdentifiedAnnotation.class, sent);
+      for (IdentifiedAnnotation identifiedAnnotation : entities)
       {
-        continue;
-      }
-      IdentifiedAnnotation target = identifiedAnnotation;
+        if (!(identifiedAnnotation instanceof EntityMention || identifiedAnnotation instanceof EventMention))
+        {
+          continue;
+        }
+        IdentifiedAnnotation target = identifiedAnnotation;
 
-      List<Feature> feats = new ArrayList<>();
+        List<Feature> feats = new ArrayList<>();
 
-      feats.addAll(this.seqExtractor.extract(jCas, target));
-      Instance<String> instance = new Instance<>(feats);
-      
-      if(this.isTraining()){
-        int polarity = target.getPolarity();
-        int uncertainty = target.getUncertainty();
-        boolean generic = target.getGeneric();
-        String subject = target.getSubject();
-        int history = target.getHistoryOf();
-        boolean conditional = target.getConditional();
+//        feats.addAll(this.seqExtractor.extract(jCas, target, sent));
+        feats.addAll(this.seqExtractor.extract(jCas, target));
+        Instance<String> instance = new Instance<>(feats);
 
-        String label = String.format("polarity=%d uncertainty=%d generic=%s subject=%s history=%d conditional=%s", polarity, uncertainty, generic, subject, history, conditional);
-        instance.setOutcome(label);
-        this.dataWriter.write(new Instance<>(label, feats));
-      }else{
-        String label = this.classifier.classify(feats);
-        String[] labels = label.split(" ");
-        int polarity = Integer.valueOf(labels[0].split("=")[1]);
-        int uncertainty = Integer.valueOf(labels[1].split("=")[1]);
-        boolean generic = Boolean.valueOf(labels[2].split("=")[1]);
-        String subject = labels[3].split("=")[1];
-        int history = Integer.parseInt(labels[4].split("=")[1]);
-        boolean conditional = Boolean.valueOf(labels[5].split("=")[1]);
-        
-        target.setPolarity(polarity);
-        target.setUncertainty(uncertainty);
-        target.setGeneric(generic);
-        target.setSubject(subject);
-        target.setHistoryOf(history);
-        target.setConditional(conditional);
+        if(this.isTraining()){
+          int polarity = target.getPolarity();
+          int uncertainty = target.getUncertainty();
+          boolean generic = target.getGeneric();
+          String subject = target.getSubject();
+          int history = target.getHistoryOf();
+          boolean conditional = target.getConditional();
+
+          String label = String.format("polarity=%d uncertainty=%d generic=%s subject=%s history=%d conditional=%s", polarity, uncertainty, generic, subject, history, conditional);
+          instance.setOutcome(label);
+          this.dataWriter.write(new Instance<>(label, feats));
+        }else{
+          String label = this.classifier.classify(feats);
+          String[] labels = label.split(" ");
+          int polarity = Integer.valueOf(labels[0].split("=")[1]);
+          int uncertainty = Integer.valueOf(labels[1].split("=")[1]);
+          boolean generic = Boolean.valueOf(labels[2].split("=")[1]);
+          String subject = labels[3].split("=")[1];
+          int history = Integer.parseInt(labels[4].split("=")[1]);
+          boolean conditional = Boolean.valueOf(labels[5].split("=")[1]);
+
+          target.setPolarity(polarity);
+          target.setUncertainty(uncertainty);
+          target.setGeneric(generic);
+          target.setSubject(subject);
+          target.setHistoryOf(history);
+          target.setConditional(conditional);
+        }
       }
     }
   }  
