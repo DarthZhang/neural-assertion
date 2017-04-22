c4b2db6a6d54dab6dfaa359f0aba691e0f0149f3
tmill <tim.miller@gmail.com>
2017-04-22 06:31:48 -0400

    Add extra parameter to singletask defs file.

--- scripts/keras/singletask/defs.sh
+++ scripts/keras/singletask/defs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export CTAKES_NEURAL=/Users/tmill/Projects/apache-ctakes/ctakes-neural
 export TRAIN_SCRIPT=assertion_split_cnn_train.py
 export PREDICT_SCRIPT=assertion_split_cnn_predict.py
 
